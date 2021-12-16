a369919b72cd70469b524e7be2ab025f41d354f0
HankHint <hankhint@gmail.com>
2021-12-15 19:18:55 -0500

    added comments for explaining how sequelize functions

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,8 +2,13 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+
+// if it exists we are in the heroku env, if it doesn't we are on the local machine
+// validates that sequelize is not NULL
 const sequelize = process.env.JAWSDB_URL
+//if true set to process
   ? new Sequelize(process.env.JAWSDB_URL)
+  // if false go to this branch
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
@@ -11,5 +16,17 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
+
+  var number = true ? 5: 10
+ 
+  var number;
+  // if input is false then number = 10
+  // if input is true then number = 5
+  if (false) 
+  {
+    number = 5;
+  } else {
+    number = 10;
+  }
 
 module.exports = sequelize;
--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 
+// we are calling require, passing in the file name, it is returning module.exports
+//and assigning it to the sequelize variable
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
